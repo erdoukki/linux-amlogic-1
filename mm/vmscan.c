ead4611b4dfdfb8d8fd53da956049988435b600e
Javier Marcet <javier@marcet.info>
2015-03-17 22:50:50 +0100

    Silence screamers in mm/vmscan
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2820,8 +2820,10 @@ loop_again:
 				zone_clear_flag(zone, ZONE_CONGESTED);
 			}
 		}
+		/*
 		printk(KERN_DEBUG"%s end zone=%d, pgdat_is_balanced=%d\n", __func__,
 			   end_zone, pgdat_is_balanced);
+		*/
 		if (i < 0) {
 			pgdat_is_balanced = true;
 			goto out;
@@ -2921,7 +2923,7 @@ loop_again:
 				 */
 				zone_clear_flag(zone, ZONE_CONGESTED);
 		}
-		printk(KERN_DEBUG"%s, nr_reclaimed=%lu\n",__func__, sc.nr_reclaimed);
+		//printk(KERN_DEBUG"%s, nr_reclaimed=%lu\n",__func__, sc.nr_reclaimed);
 		/*
 		 * If the low watermark is met there is no need for processes
 		 * to be throttled on pfmemalloc_wait as they should not be
