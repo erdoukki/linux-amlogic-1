AnimeROM/android_kernel_motorola_msm8226

Enhance power efficiency: Series of patches from NVIDIA on some critical blocking code paths to allow the CPU to relax instead of busy spinning thus lowering the load_average and lowering power consumption.
Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

kernel/smp.c
@@ -12,6 +12,7 @@
 #include <linux/gfp.h>
 #include <linux/smp.h>
 #include <linux/cpu.h>
+#include <asm/relaxed.h>
 
 #ifdef CONFIG_USE_GENERIC_SMP_HELPERS
 static struct {
@@ -127,8 +128,8 @@ void __init call_function_init(void)
 static void csd_lock_wait(struct call_single_data *data)
 {
 	set_csd_lock_waiting_flag();
-	while (data->flags & CSD_FLAG_LOCK)
-		cpu_relax();
+	while (cpu_relaxed_read_short(&data->flags) & CSD_FLAG_LOCK)
+		cpu_read_relax();
 	clear_csd_lock_waiting_flag();
 }
 

erdoukki/linux-amlogic-1

kernel/smp.c
@@ -12,6 +12,7 @@
 #include <linux/gfp.h>
 #include <linux/smp.h>
 #include <linux/cpu.h>
+#include <asm/relaxed.h>
 
 #include "smpboot.h"
 
@@ -102,8 +103,8 @@ void __init call_function_init(void)
  */
 static void csd_lock_wait(struct call_single_data *csd)
 {
-	while (csd->flags & CSD_FLAG_LOCK)
-		cpu_relax();
+	while (cpu_relaxed_read_short(&csd->flags) & CSD_FLAG_LOCK)
+		cpu_read_relax();
 }
 
 static void csd_lock(struct call_single_data *csd)